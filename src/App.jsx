98a39523beb6157924eaffbfa23361693cbe664b
adminIntan <admin@intanglass.com>
2026-09-05 02:07:31 +0700

    Add Vercel Web Analytics and change title

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
 import PersonalDev from './components/PersonalDev'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react' // 1. Import paket
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Analytics /> {/* 2. Tambahkan komponen ini */}
+  </React.StrictMode>
+)
 
 export default function App() {
   return (
